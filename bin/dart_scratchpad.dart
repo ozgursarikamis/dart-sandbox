d5e9fa198980cc1631ff76963199b859967b7789
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2023-03-17 01:31:25 +0000

    first commit

---
+++ bin/dart_scratchpad.dart
@@ -0,0 +1,5 @@
+import 'package:dart_scratchpad/dart_scratchpad.dart' as dart_scratchpad;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_scratchpad.calculate()}!');
+}
---
+++ test/dart_scratchpad_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_scratchpad/dart_scratchpad.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
